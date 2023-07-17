4cbca41c9a8e18df8329e8141df1be9c223d61f0
srijanshankar <srijanshankar255@gmail.com>
2023-07-17 07:38:54 +0000

    Upto Q3 TelecomService

--- srijan_telecom.sql
+++ srijan_telecom.sql
@@ -7,3 +7,11 @@ select "Customer Name" from TELECOM_CUSTOMER where PRODUCT = 'Digital Subscriber
 2. Write a SQL Query to list a customer id, customer name whose name starts with 'sa'
 */
 select CUSTOMERID, "Customer Name" from TELECOM_CUSTOMER WHERE "Customer Name" like 'sa%';
+
+/* 
+3. Write a SQL Query to list the Customer IDs and names for customers belonging to the gold customer segment.
+Note:- After discussing with team we found that there is no gold word in customer segment rather it is present in service segment.
+Mentioned below is the query accordingly. 
+*/
+select CUSTOMERID, "Customer Name" from TELECOM_CUSTOMER where "Service Segment" = 'Gold';
+
