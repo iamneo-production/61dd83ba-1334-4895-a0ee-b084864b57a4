eb6255b1228dd768312dc9a634a45514a876e6a6
srijanshankar <srijanshankar255@gmail.com>
2023-07-17 07:18:11 +0000

    Question 1 - TelecomService

--- srijan_telecom.sql
+++ srijan_telecom.sql
@@ -2,3 +2,4 @@
 1. Write a SQL Query to find a list of customer names who are using the product Digital Subscriber Line.
 */
 select "Customer Name" from TELECOM_CUSTOMER where PRODUCT = 'Digital Subscriber Line';
+
