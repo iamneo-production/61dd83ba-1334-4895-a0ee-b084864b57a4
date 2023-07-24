709581e0a2de5d53d74d70772b12635cb4ae6310
avinash-gandham <avinash62.aa@gmail.com>
2023-07-24 05:37:57 +0000

    WORKSPACE QUERIES

--- avinash.sql
+++ avinash.sql
@@ -1,21 +1,21 @@
-/*SQL query to find a list of customer names who are using the product digital subscriber line */
-select "Customer Name" from TELECOM_CUSTOMER
-where product='Digital Subscriber Line';
+/*SQL query to find a list of customer names who are using the product digital subscriber line */ 
+SELECT "Customer Name" FROM TELECOM_CUSTOMER
+WHERE PRODUCT='Digital Subscriber Line';
 
 /* SQL query to a list a customer id,customer name whose name starts with 'sa' */
-select CustomerId, "Customer Name" from TELECOM_CUSTOMER
-where "Customer Name" like 'sa%';
+SELECT CUSTOMERID, "Customer Name" FROM TELECOM_CUSTOMER
+WHERE "Customer Name" LIKE 'sa%';
 
 
 /* SQL query to list the customer ids and names for customers belonging to the gold customer segment */
-select CustomerId, "Customer Name" from TELECOM_CUSTOMER
-where "Service Segment"='Gold';
+SELECT CUSTOMERID, "Customer Name" FROM TELECOM_CUSTOMER
+WHERE "Service Segment"='Gold';
 
 /* SQL query to count the customer list product-wise */
-select product, count(product) from TELECOM_CUSTOMER
-group by product;
+SELECT PRODUCT, COUNT(PRODUCT) FROM TELECOM_CUSTOMER
+GROUP BY PRODUCT;
 
 
 /* SQL query to list the customer name of zone 'Mountain' */
-select "Customer Name" from TELECOM_CUSTOMER
-where ZONE='Mountain';
+SELECT "Customer Name" FROM TELECOM_CUSTOMER
+WHERE ZONE='Mountain';
