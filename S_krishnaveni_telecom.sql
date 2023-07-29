f9772d426912effd5cd0b5bb5ae94f6c5f7d21e8
sriramwarkrishnaveni <krishnaveni40405@gmail.com>
2023-07-29 07:45:34 +0000

    commit_file

---
+++ S_krishnaveni_telecom.sql
@@ -0,0 +1,14 @@
+/*1Q)write a SQL query to find a list of customer names who are using the product Digital Subscriber Line.*/
+SELECT "Customer Name" FROM TELECOM WHERE PRODUCT='Digital Subscriber Line';
+
+/*2Q)write a SQL query to list a customer id,customer name whose name starts with 'sa'.*/
+SELECT CUSTOMERID,"Customer Name" FROM TELECOM WHERE "Customer Name" LIKE 'sa%';
+
+/*3Q)write SQL query to list the customer ids and names for customers belonging to the gold customer segment.*/
+SELECT CUSTOMERID,"Customer Name" FROM TELECOM WHERE "Service Segment"='Gold';
+
+/*4Q)write a SQL query to count the customer list product wise.*/
+SELECT PRODUCT,COUNT(PRODUCT) FROM TELECOM GROUP BY PRODUCT;
+
+/*5Q)write a SQL query to list the customer name zone 'Mountain'.*/
+SELECT "Customer Name" FROM TELECOM WHERE ZONE LIKE 'M%';
